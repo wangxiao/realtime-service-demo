1d0e9e6d76d83ce964bbc2069964718e51bffe93
leeyeh <lee55962698@gmail.com>
2015-12-01 18:16:31 +0800

    Response with 503 when no staff online.

--- cloud.js
+++ cloud.js
@@ -6,18 +6,25 @@ AV.Cloud.define('start-conv', function (req, res) {
   // 为当前客户生成 UUID
   var customId = uuid.v4();
   // 找到最闲的客服
-  var query = new AV.Query('Staff').ascending('concurrent');
-  query.first().then(function (staff) {
+  var query = new AV.Query('Staff').limit(20).ascending('concurrent');
+  query.find().then(function (staffs) {
+    return getFirstOnlineStaff(staffs);
+  }).then(function (staff) {
+    if (!staff) {
+      var error = new Error('No staff online');
+      error.statusCode = 503;
+      return Promise.reject(error);
+    }
     var staffId = staff.get('clientId');
     console.log('custom [' + customId + '] assigned to staff [' + staffId + ']');
     // 客服同时处理客户数 +1
     staff.increment('concurrent', 1).save();
     // 创建一个包含当前客户与选中客服的会话
     return createConversation([customId, staffId]);
-  }).then(function(conv) {
+  }).then(function (conv) {
     console.log('new conversation [' + conv.objectId + '] created');
     // 在创建好的会话中代替客户发送问题
-    return sendQuestion(req.params.question, conv.objectId, customId).then(function() {
+    return sendQuestion(req.params.question, conv.objectId, customId).then(function () {
       // 返回客户端需要的数据：会话 ID 与顾客 ID
       return res.success({
         convId: conv.objectId,
@@ -26,7 +33,7 @@ AV.Cloud.define('start-conv', function (req, res) {
     });
   }).catch(function (e) {
     console.log(e);
-    res.error(e.message);
+    res.error(e);
   });
 });
 
@@ -66,5 +73,30 @@ function sendQuestion(text, convId, fromPeer) {
       'X-LC-Key': process.env.LC_APP_MASTER_KEY + ',master'
     },
     json: true
+  });
+}
+
+function getFirstOnlineStaff(staffs) {
+  // 获取客服的在线状态
+  return request({
+    method: 'POST',
+    uri: 'https://leancloud.cn/1.1/rtm/online',
+    body: {
+      peers: staffs.map(function (staff) {
+        return staff.get('clientId');
+      })
+    },
+    headers: {
+      'X-LC-Id': process.env.LC_APP_ID,
+      // 该操作需要使用 master key
+      'X-LC-Key': process.env.LC_APP_MASTER_KEY + ',master'
+    },
+    json: true
+  }).then(function (res) {
+    // 返回在线的最闲的客服
+    var clientId = res.results[0];
+    return staffs.reduce(function (prev, curr) {
+      return curr.get('clientId') === clientId ? curr : prev;
+    }, null);
   });
 }
